79a5d5c31856d0c054440d3750ac85ca18425b12
purvinsm <110044066+purvinsm@users.noreply.github.com>
2026-05-04 16:29:58 +0300

    Update __init__.py

--- checks/   mytask/check50/__init__.py
+++ checks/   mytask/check50/__init__.py
@@ -1,1 +1,17 @@
+from check50 import *
 
+class Hello(Check):
+    @check()
+    def exists(self):
+        """hello.py exists"""
+        self.require("hello.py")
+
+    @check("exists")
+    def compiles(self):
+        """hello.py runs without error"""
+        self.spawn("python3 hello.py").exit(0)
+
+    @check("compiles")
+    def correct_output(self):
+        """prints Hello, World!"""
+        self.spawn("python3 hello.py").stdout("Hello, World!\n", "Hello, World!")
